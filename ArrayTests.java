ab755e88f4f583552358a1e3d8f3cfe4d2b52b17
pmckenna2425 <130352092+pmckenna2425@users.noreply.github.com>
2023-04-24 23:16:41 -0700

    Fixed the bugs
    the reverseInPlage method worked for the first half of the elements of the array, but beyond that it was working with already reversed elements.
    The reversed method was just setting elements in the original array to 0 (elements in newArray) rather than setting elements in newArray to elements in arr.

--- ArrayTests.java
+++ ArrayTests.java
@@ -18,9 +18,9 @@ public class ArrayTests {
   
   @Test
   public void testReverseInPlace2() {
-	  int[] input = {1,2,3,4,5,6};
+	  int[] input = {1,2,3,4,5,6,7};
 	  ArrayExamples.reverseInPlace(input);
-	  assertArrayEquals(new int[] {6,5,4,3,2,1}, input);
+	  assertArrayEquals(new int[] {7,6,5,4,3,2,1}, input);
   }
   
   @Test
