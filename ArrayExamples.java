02282bfd6af9d61ef5f860f7b3f5c5b125c841d0
pmckenna2425 <130352092+pmckenna2425@users.noreply.github.com>
2023-05-17 00:04:38 -0700

    Update ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -3,16 +3,33 @@
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
+  /* The bug in the initial method was that as it went through the array setting the first elements to the 
+   * last elements, it didn't also replace the last elements with the first elements. So by the time I was more than halfway
+   * through the array, the earlier elements for it to be sent to had already been changed. 
+   */
+
   static void reverseInPlace(int[] arr) {
     for(int i = 0; i < arr.length/2; i += 1) {
       int num = arr[i];
       arr[i] = arr[arr.length - 1 - i];
       arr[arr.length - 1 - i] = num; 
     }
-  }
+  } 
+  
+  /* 
+  static void reverseInPlace(int[] arr) {
+    for(int i = 0; i < arr.length; i += 1) {
+      arr[i] = arr[arr.length - i - 1];
+    }
+  } */
+
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
+  /* The problem with the initial method was merely the order of the for loop body. It put arr[i] on the left 
+  of the = sign, marking arr[i] as the value to be changed to newArr[arr.length - 1 - i] (which was 0 because 
+  newArr had just been initialized). 
+   */
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
